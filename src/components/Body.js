52481ce7043e92450e9d8bae2fddac19b7270d7e
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-11-19 14:20:54 +1100

    Create footer and body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,108 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "../styles/Body.css"
+import image1 from "../assets/pexels-aron-visuals-1743165.jpg";
+import image2 from "../assets/pexels-eberhard-grossgasteiger-1366919.jpg";
+import image3 from "../assets/pexels-eberhard-grossgasteiger-2310641.jpg";
+import image4 from "../assets/pexels-pixabay-147411.jpg";
+import image5 from "../assets/pexels-simon-berger-1323550.jpg";
+import image6 from "../assets/pexels-tobi-675764.jpg";
 
 const Body = () => {
     return (
         <div id="body">
-            Body
+            <h1>My Work</h1>
+            <div class="project-container">
+                <div class="project">
+                    <img src={image1} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+                <div class="project">
+                    <img src={image2} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+                <div class="project">
+                    <img src={image3} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+                <div class="project">
+                    <img src={image4} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+                <div class="project">
+                    <img src={image5} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+                <div class="project">
+                    <img src={image6} alt="sakd"></img>
+                    <div class="project-overlay">
+                        <div class="heading">
+                            <h2>Project Name</h2>
+                            <div class="links">
+                                <i class="devicon-github-original"></i>
+                                <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                            </div>
+                        </div>
+                        <div class="description">
+                            Short description of this project. A few sentences will do.
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "../styles/Footer.css";
+import profile from '../assets/pexels-lisa-fotios-2935956.jpg'
 
 const Footer = () => {
     return (
         <footer>
-            Footer
+            <div className="body">
+                <h2>Contact Me</h2>
+                <p>Please get in touch if you think our work could be mutally beneficial.</p>
+                <div className="address">
+                    <p>1234 Random Road</p>
+                    <p>Random Town, California 12345</p>
+                </div>
+                <div className="phone-number">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>555-555-5555</p>
+                </div>
+                <div className="email">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>notarealemail.gmail.com</p>
+                </div>
+                <div className="links">
+                    <i class="devicon-github-original"></i>
+                    <i class="devicon-linkedin-plain"></i>
+                    <i class="devicon-facebook-plain"></i>
+
+                </div>
+            </div>
+            <img src={profile} alt="profile"></img>
         </footer>
     )
 }
